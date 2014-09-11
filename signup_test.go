0a1326a0b050b05dd1657b075f750dd97b505b14
thepumpkin1979 <johan@heapsource.com>
2014-09-11 11:50:24 -0500

    signup tests

---
+++ signup_test.go
@@ -0,0 +1,46 @@
+package bithavocid
+
+import (
+	"fmt"
+	"net/url"
+	"testing"
+)
+
+func TestSuccessSignup(t *testing.T) {
+	twc := newTestWebClient()
+	form := url.Values{}
+	form.Set("email", "bill@gates.com")
+	form.Set("fullname", "Bill G")
+	form.Set("password", "msdos")
+	form.Set("password_confirmation", "msdos")
+	resultObject := &baseResult{}
+
+	twc.subscribe("myAppId", "sign-up", form, resultObject, nil)
+
+	client := newClient("myAppId", twc)
+	err := client.SignUp(SignUp{
+		Email:    "bill@gates.com",
+		Password: "msdos",
+		Fullname: "Bill G",
+	})
+	assertf(t, err == nil, "err should be null")
+}
+
+func TestFailedSignup(t *testing.T) {
+	twc := newTestWebClient()
+	form := url.Values{}
+	form.Set("email", "bill@gates.com")
+	form.Set("fullname", "")
+	form.Set("password", "msdos")
+	form.Set("password_confirmation", "msdos")
+	resultObject := &baseResult{}
+	twc.subscribe("myAppId", "sign-up", form, resultObject, fmt.Errorf("fullname: fullname is required"))
+
+	client := newClient("myAppId", twc)
+	err := client.SignUp(SignUp{
+		Email:    "bill@gates.com",
+		Password: "msdos",
+	})
+	assertf(t, err != nil, "error should not be null")
+	assertf(t, err.Error() == "fullname: fullname is required", "value required error should be given")
+}
